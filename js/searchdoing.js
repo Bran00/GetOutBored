48a24d4b5782122ed18c086dde3175b7e473eeaa
Bran00 <86134682+Bran00@users.noreply.github.com>
2022-12-19 12:13:40 -0300

    Update searchdoing.js

--- js/searchdoing.js
+++ js/searchdoing.js
@@ -1,6 +1,6 @@
 export class Activity {
   static search() {
-    const endpoint = `http://www.boredapi.com/api/activity/`
+    const endpoint = `https://www.boredapi.com/api/activity/`
     
     return fetch(endpoint)
     .then(data => data.json())
